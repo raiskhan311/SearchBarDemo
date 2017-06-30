fc8cc66ca9ee8744cdb3fccebbad34555241c439
SudhakarGVVN <sudhakar@zanjo.io>
2017-06-30 07:46:16 +0530

    search bar demo on tableview

--- SearchBarDemo/ViewController.swift
+++ SearchBarDemo/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate,UISearchBarDelegate {
+    
+    @IBOutlet weak var tbl_TableView: UITableView!
+    
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
+    var searching:Bool! = false // this is for search is on or not that identifier
+    var originalArray = [String]()
+    var searchingDataArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        originalArray = ["Intex","Motorola", "Vivo", "Haier", "Samsung", "Nokia","Sony", "Apple","Mi","Micromax"]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching == true
+        {
+            return searchingDataArray.count
+        }
+        else
+        {
+            return originalArray.count
+        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if searching == true
+        {
+            cell.textLabel?.text = searchingDataArray[indexPath.row]
+        }
+        else
+        {
+            cell.textLabel?.text = originalArray[indexPath.row]
+        }
+        return cell
+    }
+    
+    @objc(searchBar:textDidChange:) func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        searchingDataArray = originalArray.filter({(text) ->
+            Bool in
+            let temp: NSString = text as NSString
+            let range = temp.range(of: searchText, options:
+            NSString.CompareOptions.caseInsensitive)
+            return range.location != NSNotFound
+        })
+        if (searchingDataArray.count == 0)
+        {
+            searching = false
+        }
+        else
+        {
+            searching = true
+        }
+        self.tbl_TableView.reloadData()
+    }
 }
 
